d6b046e14aeccd7f0b9bc6b706e6472624886fe3
SeanLovesTea <sean.m.concannon@gmail.com>
2022-08-14 15:47:10 +0100

    Basic styles and calculater function, working on calc accepting double digits.

--- main.js
+++ main.js
@@ -1,3 +1,67 @@
-function add(a,b){
-    return a+b
+const output = document.querySelector('.output');
+const numBtn = document.querySelectorAll('#number');
+const operatorBtn = document.querySelectorAll('#operator');
+const clearBtn = document.querySelector('.clear-btn');
+const number = document.querySelector('number');
+// const operator =document.querySelector('operator');
+
+let currentNumber;
+let previousNumber;
+let operator;
+
+numBtn.forEach(numBtn => {
+    numBtn.addEventListener("click", (e) => {
+        const displayValue = e.target.textContent;
+        output.innerHTML += displayValue;
+        currentNumber += displayValue;
+    });
+});
+
+
+operatorBtn.forEach(operatorBtn => {
+    operatorBtn.addEventListener("click", (e) => {
+        const displayValue = e.target.textContent;
+        if (e.target.id == "operator"){
+            operator = e.target.textContent
+            previousNumber = currentNumber;
+            output.innerHTML += displayValue;
+        }
+    });
+});
+
+function calculate(){
+    console.log("pre " +previousNumber + "cur " +currentNumber)
+    previousNumber = Number(previousNumber)
+    currentNumber = Number(currentNumber)
+    if(operator === "+"){
+        console.log(previousNumber + currentNumber)
+        return previousNumber + currentNumber
+    }
+    if(operator === "-"){
+        console.log(previousNumber - currentNumber)
+        return previousNumber - currentNumber
+        
+    }
+    if(operator === "*"){
+        console.log(previousNumber * currentNumber)
+        return previousNumber * currentNumber
+    }
+    if(operator === "/"){
+        console.log(previousNumber / currentNumber)
+        return previousNumber / currentNumber
+    }
 }
+
+let clearDisplay = function(){
+    console.log("clear function")
+    output.innerHTML = "";
+}
+/* take input 1 
+    store it
+    take operator 
+    store it
+    take input 2
+    store it
+    then run function 
+    if operator = "" then 
+    a formula with the inputs*/
