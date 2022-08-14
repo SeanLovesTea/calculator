4ef911628883653a8ef4fc37f404e0a0d9844d29
SeanLovesTea <sean.m.concannon@gmail.com>
2022-08-14 17:42:13 +0100

    all working bar can add multiple operators at once

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const output = document.querySelector('.output');
+const prevOut = document.querySelector('.output-previous');
 const numBtn = document.querySelectorAll('#number');
 const operatorBtn = document.querySelectorAll('#operator');
 const clearBtn = document.querySelector('.clear-btn');
-const number = document.querySelector('number');
-// const operator =document.querySelector('operator');
+// const number = document.querySelector('number');
+// // const operator =document.querySelector('operator');
 
 let currentNumber;
 let previousNumber;
@@ -11,29 +12,38 @@ let operator;
 
 numBtn.forEach(numBtn => {
     numBtn.addEventListener("click", (e) => {
+        console.log("elength : " +e.target.textContent.length)
+        if(output.textContent.length <= 11){
         const displayValue = e.target.textContent;
         output.innerHTML += displayValue;
-        currentNumber = displayValue;
+        currentNumber = output.textContent;
+        console.log("number : " +output.textContent)
+        }
     });
 });
-
 
 operatorBtn.forEach(operatorBtn => {
     operatorBtn.addEventListener("click", (e) => {
         const displayValue = e.target.textContent;
-        if (e.target.id == "operator"){
-            operator = e.target.textContent
-            previousNumber = currentNumber;
-            output.innerHTML += displayValue;
-        }
+        operator = e.target.textContent
+        previousNumber = currentNumber;
+        prevOut.innerHTML += displayValue;
+        prevOut.innerHTML += output.innerHTML;
+        output.innerHTML = "";
     });
 });
 
 function calculate(){
     console.log("pre " +previousNumber + "cur " +currentNumber)
+
     let answer; 
     previousNumber = Number(previousNumber)
     currentNumber = Number(currentNumber)
+   
+    if(operator === ""){
+        console.log(currentNumber)
+        answer = currentNumber
+    }
     if(operator === "+"){
         console.log(previousNumber + currentNumber)
         answer = previousNumber + currentNumber
@@ -41,7 +51,6 @@ function calculate(){
     if(operator === "-"){
         console.log(previousNumber - currentNumber)
         answer = previousNumber - currentNumber
-        
     }
     if(operator === "*"){
         console.log(previousNumber * currentNumber)
@@ -52,12 +61,17 @@ function calculate(){
         answer = previousNumber / currentNumber
     }
     clearDisplay();
-    output.innerHTML =  answer;
+    output.innerHTML = ""
+    prevOut.innerHTML = answer;
+
+    currentNumber = ""
+    previousNumber =""
     return answer;
 }
 
 let clearDisplay = function(){
     output.innerHTML = "";
+    prevOut.innerHTML = "";
 }
 /* take input 1 
     store it
